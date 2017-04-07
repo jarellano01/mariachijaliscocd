5a93a843581e3d00b19b632095f8efef19987b81
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2017-04-07 13:03:14 -0700

    added rentals carousel

--- src/components/sections/Rentals.js
+++ src/components/sections/Rentals.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Col} from 'react-bootstrap';
+import {Col,Carousel} from 'react-bootstrap';
 import Section from '../Section';
 
 class Rentals extends Component {
@@ -7,14 +7,18 @@ class Rentals extends Component {
         return(
             <Section id="about">
 
-                <Col xs={12} md={8} mdPush={4}>
+                <Col xs={12} md={6} mdPush={6}>
                     <h1 className="title">24 Hour Rentals</h1>
                     <p>
                         ***CrystalArts Dance Academy has a 24 hour cancellation/change policy.  If you cancel or reschedule less then 24 hours before your booking is scheduled to begin, you will be charged for the full booking.  This policy also applies to "same day" bookings.  If you are booking a room for the same day or less then 24 hours in advance, that booking will be considered a confirmed booking and will not be able to be changed or canceled without payment.
                     </p>
                 </Col>
-                <Col xs={12} md={4} mdPull={8} className="text-center">
-                    <img src="/images/studiopic.jpeg" style={{width: '100%'}} alt=""/>
+                <Col xs={12} md={6} mdPull={6} className="text-center">
+                    <Carousel>
+                        <Carousel.Item>
+                            <img width={900} height={500} alt="900x500" src="/images/studiopic.jpeg"/>
+                        </Carousel.Item>
+                    </Carousel>
                 </Col>
             </Section>
         )
