9f3bdab5011c1702321d3619933385b21e981025
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2017-05-02 16:52:34 -0700

    added flyer

--- src/components/fullpages/events.js
+++ src/components/fullpages/events.js
@@ -29,24 +29,10 @@ class Events extends Component {
         let {hidden} = this.state;
         return(
             <Section className={`full-page ${hidden ? null: 'unhide'}`} id="event">
-                <Col xs={12} md={8} mdPush={4}>
-                    <h1 className="title">Our Story</h1>
-                    <Link to="/">Back Home</Link>
-                    <p>
-                        CrystalArts Dance Academy is where creativity and diversity exists! Our mission is to
-                        produce well trained dancers in both social and stage levels. Our goal is to provide a
-                        platform for everyone who is developing a new hobby, who already has the desire to learn and
-                        CRYSTALLIZE their arts to show his/hers talent on stage.
-                    </p>
+                <Col xs={12} md={8} mdPush={2}>
+                    <img src="/flyers/pop-up-5-13.jpeg" className="img-responsive flyer" alt=""/>
                 </Col>
-                <Col xs={12} md={4} mdPull={8} className="text-center">
-                    <img src="/images/crystal-pose-tony.jpg" style={{width: '80%'}} alt=""/>
-                </Col>
-                <Link to="/">
-                    <div className="show-events">
-                        Close
-                    </div>
-                </Link>
+                
             </Section>
         )
     }
